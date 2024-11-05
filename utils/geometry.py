c4bf858dee3e67481a26aa46ecc973810e1a4464
Staggs <tannerstaggs@ufl.edu>
2024-11-05 08:55:02 -0500

    fixing markov chain and adding utils

--- utils/geometry.py
+++ utils/geometry.py
@@ -1,3 +1,4 @@
+from geographiclib.geodesic import Geodesic
 import shapely
 
 # bbox is a list of tuples and point is a tuple
@@ -5,3 +6,15 @@ def point_in_bbox(bbox: list, point: tuple):
     pt = shapely.Point(point)
     shapely_box = shapely.Polygon(bbox)
     return shapely_box.contains(pt)
+
+# Return a "N" "E" or "O" based on bearing
+def get_direction(lat1: float, lon1: float, lat2: float, lon2: float):
+    result = Geodesic.Inverse(lat1=lat1, lon1=lon1, lat2=lat2, lon2=lon2)
+    bearing = result["azi1"]
+
+    if bearing > 45 and bearing <= 135:
+        return "E"
+    elif bearing > 135 and bearing < 315:
+        return "O"
+    else:
+        return "N"
