37b8601db74cb41bf1a699ce0561903ac48eb871
karleybaringer <104109889+karleybaringer@users.noreply.github.com>
2024-11-14 12:34:13 -0500

    Create simulation.py

---
+++ utils/simulation.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def monteCarloPathPredict(probs):
+
+    rng = np.random.default_rng() # initialize Generator
+    return rng.choice(np.array(range(len(probs))), p=probs) # will output index of selection in row
+
+
+
